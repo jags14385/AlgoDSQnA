e901d5365f96854b072bd6679e306a51edb9cd30
Jags <jags14385@users.noreply.github.com>
2020-07-15 17:40:36 +1000

    Find Number of Occurences

---
+++ src/main/java/numOfOccurences/FindNumberOfOccurences.java
@@ -0,0 +1,71 @@
+package numOfOccurences;
+
+import java.util.Arrays;
+
+public class FindNumberOfOccurences {
+
+    public boolean IsNull(int[] arr) {
+        return arr == null || arr.length == 0;
+    }
+
+    public long findNumberOfOccurences(int[] arr, int n) {
+        if (this.IsNull(arr))
+            return 0;
+
+        long counter = 0;
+        for (int num : arr) {
+            if (num == n)
+                counter++;
+        }
+        return counter;
+    }
+
+    public long findNumberOfOccurencesInASortedArray(int[] arr, int n) {
+        if (this.IsNull(arr))
+            return 0;
+
+        long counter = 0;
+        int leftStart = 0;
+        int rightEnd = arr.length - 1;
+        int middle = (leftStart + rightEnd) / 2;
+
+        while (leftStart < rightEnd && leftStart >= 0) {
+            middle = (leftStart + rightEnd) / 2;
+            if (arr[middle] == n) {
+                counter++;
+                break;
+            } else if (arr[middle] < n) {
+                leftStart = middle + 1;
+            } else if (arr[middle] > n) {
+                rightEnd = middle - 1;
+            }
+        }
+        int index = middle - 1;
+        while (index >= 0) {
+            if (arr[index] == n) {
+                counter++;
+                index--;
+            } else break;
+        }
+
+        index = middle + 1;
+        while (index <= arr.length - 1 && index >= 0) {
+            if (arr[index] == n) {
+                counter++;
+                index++;
+            } else break;
+        }
+        
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 1, 1, 1};
+        long frequency = new FindNumberOfOccurences().findNumberOfOccurences(arr, 1);
+        System.out.println(frequency);
+        Arrays.sort(arr);
+        frequency = new FindNumberOfOccurences().findNumberOfOccurencesInASortedArray(arr, 1);
+        System.out.println(frequency);
+    }
+
+}
